4177f1ffe9fe508d44e0667aaa82087f3930e566
tilalapriyank <priyankpatel2764@gmail.com>
2025-01-16 15:43:04 +0530

    last 16/01

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,82 +1,10 @@
+import dotenv from "dotenv";
 
-// API key
-export const API_KEY = "5e49f223b1msh47d713ec8467bcfp186a96jsn1a7b4b20677f";
+dotenv.config();
 
-// API host
-export const API_HOST = "cricbuzz-cricket.p.rapidapi.com";
+export const API_KEY = process.env.API_KEY;
+export const API_HOST = process.env.API_HOST;
+export const BASE_URL = process.env.BASE_URL;
 
-// Base URL for the API
-export const BASE_URL = `https://${API_HOST}/`;
-
-
-// https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent
-// https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/41881
-// https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/35878/team/9
-// https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/41881/overs
-// https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/40381/hscard
-
-// https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/international
-// https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/domestic
-// https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/women
-// https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/league
-
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/international
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/domestic
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/women
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/league
-
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/archives/international
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/archives/domestic
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/archives/women
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/archives/league
-
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/3641
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/series/3636
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/3718/squads
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/3718/squads/15826
-// https://cricbuzz-cricket.p.rapidapi.com/series/v1/3718/venues
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/series/3718/points-table
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/series/3718
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/series/3718?statsType=mostRuns
-
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/international
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/domestic
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/women
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/league
-
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/2/schedule
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/2/results
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/team/2
-// https://cricbuzz-cricket.p.rapidapi.com/teams/v1/2/players
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/team/2
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/team/2?statsType=mostRuns
-
-// https://cricbuzz-cricket.p.rapidapi.com/venues/v1/45
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/venue/24
-// https://cricbuzz-cricket.p.rapidapi.com/venues/v1/45/matches
-
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/trending
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/8733/career
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/player/8733
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/8733/bowling
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/8733/batting
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/search?plrN=Tucker
-
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/index
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/detail/122025
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/cat
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/cat/5
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/topics
-// https://cricbuzz-cricket.p.rapidapi.com/news/v1/topics/349
-
-// https://cricbuzz-cricket.p.rapidapi.com/photos/v1/index
-// https://cricbuzz-cricket.p.rapidapi.com/photos/v1/detail/5374
-// https://cricbuzz-cricket.p.rapidapi.com/img/v1/i1/c231889/i.jpg
-
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen?formatType=test
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen?formatType=t20
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen?formatType=odi
-
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/iccstanding/team/matchtype/1
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/topstats
-// https://cricbuzz-cricket.p.rapidapi.com/stats/v1/topstats/0?statsType=mostRuns
+console.log("API_KEY:", API_KEY);
+console.log("BASE_URL:", BASE_URL);
