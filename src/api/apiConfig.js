2fa9941b0c149ddf1350a141c977289c8d4c1dbb
tilalapriyank <priyankpatel2764@gmail.com>
2025-01-16 15:47:11 +0530

    done

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,10 +1,5 @@
 import dotenv from "dotenv";
 
-dotenv.config();
-
-export const API_KEY = process.env.API_KEY;
-export const API_HOST = process.env.API_HOST;
-export const BASE_URL = process.env.BASE_URL;
-
-console.log("API_KEY:", API_KEY);
-console.log("BASE_URL:", BASE_URL);
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_HOST = process.env.REACT_APP_API_HOST;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
